203a5b6de0d662b544eb0d053a62ef12e81b2091
Gabriell Araujo <ZK9RX6@users.noreply.github.com>
2017-09-06 20:27:00 -0300

    Update recepcao_socket.c

--- recepcao_socket.c
+++ recepcao_socket.c
@@ -23,6 +23,7 @@
 
 unsigned char buff1[BUFFSIZE]; // buffer de recepcao
 unsigned char buff_aux;
+int position_choosed_by_player = 0;
 
 int sockd;
 int on;
@@ -57,29 +58,33 @@ int main(int argc,char *argv[])
 		//trata apenas pacotes IP (tipo 0x0800) com UDP (0X11)
 		if(buff1[12] == 8 && buff1[13] == 0 && buff1[23] == 17)
 		{
-			// impress�o do conteudo - exemplo Endereco Destino e Endereco Origem
+			// impressao do conteudo - exemplo Endereco Destino e Endereco Origem
 			printf("Destination MAC Address: %02x:%02x:%02x:%02x:%02x:%02x \n", buff1[0],buff1[1],buff1[2],buff1[3],buff1[4],buff1[5]);
 			printf("Source MAC Address: %02x:%02x:%02x:%02x:%02x:%02x \n", buff1[6],buff1[7],buff1[8],buff1[9],buff1[10],buff1[11]);
 			printf("Ether Type: %02x%02x \n", buff1[12], buff1[13]);
 			printf("Version: %02x \n", buff1[14] >> 4);
-            buff_aux = 0;
-            buff_aux = buff1[14] << 4;
-            buff_aux = buff_aux >> 4;
-            printf("IHL: %02x \n", buff_aux);
-            printf("Type of Service: %02x \n", buff1[15]);
-            printf("Total Length: %02x%02x \n", buff1[16], buff1[17]);
-            printf("Identification: %02x%02x \n", buff1[18], buff1[19]);
-            printf("Flags + Fragment Offset: %02x%02x \n", buff1[20], buff1[21]);
-            printf("Time to Live: %02x \n", buff1[22]);
-            printf("Protocol: %02x \n", buff1[23]);
-            printf("Header Checksum: %02x%02x \n", buff1[24], buff1[25]);
-            printf("Source Address: %02x%02x%02x%02x \n", buff1[26], buff1[27], buff1[28], buff1[29]);
-            printf("Destination Address: %02x%02x%02x%02x \n", buff1[30], buff1[31], buff1[32], buff1[33]);
-            printf("Source Port: %02x%02x \n", buff1[34], buff1[35]);
-            printf("Destination Port: %02x%02x \n", buff1[36], buff1[37]);
-            printf("Length: %02x%02x \n", buff1[38], buff1[39]);
-            printf("Checksum: %02x%02x \n", buff1[40], buff1[41]);
-            printf("\n");
+			buff_aux = 0;
+			buff_aux = buff1[14] << 4;
+			buff_aux = buff_aux >> 4;
+			printf("IHL: %02x \n", buff_aux);
+			printf("Type of Service: %02x \n", buff1[15]);
+			printf("Total Length: %02x%02x \n", buff1[16], buff1[17]);
+			printf("Identification: %02x%02x \n", buff1[18], buff1[19]);
+			printf("Flags + Fragment Offset: %02x%02x \n", buff1[20], buff1[21]);
+			printf("Time to Live: %02x \n", buff1[22]);
+			printf("Protocol: %02x \n", buff1[23]);
+			printf("Header Checksum: %02x%02x \n", buff1[24], buff1[25]);
+			printf("Source Address: %02x%02x%02x%02x \n", buff1[26], buff1[27], buff1[28], buff1[29]);
+			printf("Destination Address: %02x%02x%02x%02x \n", buff1[30], buff1[31], buff1[32], buff1[33]);
+			printf("Source Port: %02x%02x \n", buff1[34], buff1[35]);
+			printf("Destination Port: %02x%02x \n", buff1[36], buff1[37]);
+			printf("Length: %02x%02x \n", buff1[38], buff1[39]);
+			printf("Checksum: %02x%02x \n", buff1[40], buff1[41]);
+			//buff_aux = buff1[42];
+			buff_aux = '4';
+            position_choosed_by_player = buff_aux - '0';
+			printf("Position choosed by player: %d \n", position_choosed_by_player);
+			printf("\n");
 		}                
 	}
 }
