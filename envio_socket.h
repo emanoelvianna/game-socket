eecc17dc82596b4236522404fa5299d8a6bd36ba
Gabriell Araujo <ZK9RX6@users.noreply.github.com>
2017-09-06 20:28:46 -0300

    Update envio_socket.h

--- envio_socket.h
+++ envio_socket.h
@@ -15,32 +15,32 @@ unsigned char target_protocol_address[IP_ADDR_LEN];       // endereco_logico_des
 
 struct estrutura_pacote
 {
-    /* Cabeçalho Ethernet */
-    unsigned char target_ethernet_address[ETHERNET_ADDR_LEN]; // endereco_fisico_destino
-    unsigned char source_ethernet_address[ETHERNET_ADDR_LEN]; // endereco_fisico_origem
-    unsigned short ethernet_type;                             // tipo_protocolo_ethernet
-    
-    /* Pacote IPv4 */
-    unsigned char version;          // versao
-    unsigned char ihl;              // tamanho_cabecalho
-    unsigned char tos;              // tipo_servico
-    unsigned short tlen;            // comprimento_pacote
-    unsigned short id;              // identificacao
-    unsigned short flags;           // flags
-    unsigned short flags_offset;    // deslocamento
-    unsigned char ttl;              // tempo_de_vida
-    unsigned char protocol;         // identificacao_protocolo
-    unsigned short chsum;
-    unsigned int src;               // endereco_fisico_origem
-    unsigned int dst;               // endereco_fisico_destino
-    unsigned char *payload_ip;      // dados_ip
-    size_t payload_size_ip;
+	/* Cabeçalho Ethernet */
+	unsigned char target_ethernet_address[ETHERNET_ADDR_LEN]; // endereco_fisico_destino
+	unsigned char source_ethernet_address[ETHERNET_ADDR_LEN]; // endereco_fisico_origem
+	unsigned short ethernet_type;                             // tipo_protocolo_ethernet
 
-    /* Pacote UDP */
-    unsigned short source_port;         // porta_origem
-    unsigned short destination_port;    // porta_destino
-    unsigned short size;                // tamanho_pacote
-    unsigned short chechsum;
-    unsigned char *payload_udp;         // dados_udp
-    size_t payload_size_udp;                
+	/* Pacote IPv4 */
+	unsigned char version;          // versao
+	unsigned char ihl;              // tamanho_cabecalho
+	unsigned char tos;              // tipo_servico
+	unsigned short tlen;            // comprimento_pacote
+	unsigned short id;              // identificacao
+	unsigned short flags;           // flags
+	unsigned short flags_offset;    // deslocamento
+	unsigned char ttl;              // tempo_de_vida
+	unsigned char protocol;         // identificacao_protocolo
+	unsigned short chsum;
+	unsigned int src;               // endereco_fisico_origem
+	unsigned int dst;               // endereco_fisico_destino
+	unsigned char *payload_ip;      // dados_ip
+	size_t payload_size_ip;
+
+	/* Pacote UDP */
+	unsigned short source_port;         // porta_origem
+	unsigned short destination_port;    // porta_destino
+	unsigned short size;                // tamanho_pacote
+	unsigned short chechsum;
+	unsigned char *payload_udp;         // dados_udp
+	size_t payload_size_udp;                
 };
