9dc47a314ae824f457d11825860f90634d0c39db
emanoelvianna <emanoel.vianna@outlook.com>
2017-09-08 16:15:30 -0300

    master - parcial

--- envio_socket.c
+++ envio_socket.c
@@ -19,32 +19,46 @@
 typedef unsigned char MacAddress[MAC_ADDR_LEN];
 extern int errno;
 
-void usage(char * exec) {
-  printf("%s <IP destino> <MAC destino> <Porta>\n", exec);
+/* método para mostrar quais são os parametros esperados */
+void usage(char *exec)
+{
+	printf("%s <IP origem> <MAC origem> <IP destino> <MAC destino> <Porta>\n", exec);
 }
 
-int main(int argc, char * argv[])
+/* imprime informações para comecar o jogo */
+void informacoesIniciais()
 {
-	if (argc < 4) {
-    	usage(argv[0]);
-  	} else {
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc < 6)
+	{
+		usage(argv[0]);
+	}
+	else
+	{
 		struct estrutura_pacote pacote;
 		unsigned char buffer[BUFFER_LEN];
 
-    	printf("MAC destino: %s\n", argv[2]);
-		
-		/** montando o pacote **/
-		memcpy(&pacote.target_ethernet_address, argv[2], ETHERNET_ADDR_LEN);
-		printf("MAC destino: %02x:%02x:%02x:%02x:%02x:%02x\n",
-				   pacote.target_ethernet_address[0],
-				   pacote.target_ethernet_address[1],
-				   pacote.target_ethernet_address[2],
-				   pacote.target_ethernet_address[3],
-				   pacote.target_ethernet_address[4],
-				   pacote.target_ethernet_address[5]);
+		printf("MAC destino: %s\n", argv[2]);
 
-		/* configuracoes para o socket */		   	
-		int sockFd = 0, retValue = 0; 
+		/** montando o pacote **/
+		strcpy(source_protocol_address, argv[1]);
+		printf("IP origem: %s\n", source_protocol_address);
+		strcpy(source_hardware_address, argv[2]);
+		printf("MAC origem: %s\n", source_hardware_address);
+
+		strcpy(target_protocol_address, argv[3]);
+		printf("IP destino: %s\n", target_protocol_address);
+		strcpy(target_hardware_address, argv[4]);
+		printf("MAC destino: %s\n", target_hardware_address);
+
+		strcpy(pacote.source_port, argv[4]);
+		printf("Porta: %04x\n", ntohs(pacote.source_port));
+
+		/* configuracoes para o socket */
+		int sockFd = 0, retValue = 0;
 		struct sockaddr_ll destAddr;
 		char dummyBuf[50];
 		short int etherTypeT = htons(0x8200);
@@ -54,7 +68,8 @@ int main(int argc, char * argv[])
 		MacAddress destMac = {0x00, 0x17, 0x9A, 0xB3, 0x9E, 0x16};
 
 		/* Criacao do socket. Todos os pacotes devem ser construidos a partir do protocolo Ethernet. */
-		if((sockFd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0) {
+		if ((sockFd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0)
+		{
 			printf("Erro na criacao do socket.\n");
 			exit(1);
 		}
@@ -63,20 +78,22 @@ int main(int argc, char * argv[])
 		destAddr.sll_family = htons(PF_PACKET);
 		destAddr.sll_protocol = htons(ETH_P_ALL);
 		destAddr.sll_halen = 6;
-		destAddr.sll_ifindex = 2;  /* indice da interface pela qual os pacotes serao enviados. Eh necess�rio conferir este valor. */
+		destAddr.sll_ifindex = 2; /* indice da interface pela qual os pacotes serao enviados. Eh necess�rio conferir este valor. */
 		memcpy(&(destAddr.sll_addr), destMac, MAC_ADDR_LEN);
 
 		/* Cabecalho Ethernet */
 		memcpy(buffer, destMac, MAC_ADDR_LEN);
-		memcpy((buffer+MAC_ADDR_LEN), localMac, MAC_ADDR_LEN);
-		memcpy((buffer+(2*MAC_ADDR_LEN)), &(etherTypeT), sizeof(etherTypeT));
+		memcpy((buffer + MAC_ADDR_LEN), localMac, MAC_ADDR_LEN);
+		memcpy((buffer + (2 * MAC_ADDR_LEN)), &(etherTypeT), sizeof(etherTypeT));
 
 		/* Add some data */
-		memcpy((buffer+ETHERTYPE_LEN+(2*MAC_ADDR_LEN)), dummyBuf, 50);
+		memcpy((buffer + ETHERTYPE_LEN + (2 * MAC_ADDR_LEN)), dummyBuf, 50);
 
-		while(1) {
+		while (1)
+		{
 			/* Envia pacotes de 64 bytes */
-			if((retValue = sendto(sockFd, buffer, 64, 0, (struct sockaddr *)&(destAddr), sizeof(struct sockaddr_ll))) < 0) {
+			if ((retValue = sendto(sockFd, buffer, 64, 0, (struct sockaddr *)&(destAddr), sizeof(struct sockaddr_ll))) < 0)
+			{
 				printf("ERROR! sendto() \n");
 				exit(1);
 			}
--- envio_socket.h
+++ envio_socket.h
@@ -10,6 +10,9 @@
 #define ARPOP_REPLY 2
 
 /* declaracao das estruturas */
+unsigned char source_hardware_address[ETHERNET_ADDR_LEN]; // endereco_fisico_origem
+unsigned char source_protocol_address[IP_ADDR_LEN];       // endereco_logico_origem
+
 unsigned char target_hardware_address[ETHERNET_ADDR_LEN]; // endereco_fisico_destino
 unsigned char target_protocol_address[IP_ADDR_LEN];       // endereco_logico_destino
 
@@ -19,28 +22,28 @@ struct estrutura_pacote
     unsigned char target_ethernet_address[ETHERNET_ADDR_LEN]; // endereco_fisico_destino
     unsigned char source_ethernet_address[ETHERNET_ADDR_LEN]; // endereco_fisico_origem
     unsigned short ethernet_type;                             // tipo_protocolo_ethernet
-    
+
     /* Pacote IPv4 */
-    unsigned char version;          // versao
-    unsigned char ihl;              // tamanho_cabecalho
-    unsigned char tos;              // tipo_servico
-    unsigned short tlen;            // comprimento_pacote
-    unsigned short id;              // identificacao
-    unsigned short flags;           // flags
-    unsigned short flags_offset;    // deslocamento
-    unsigned char ttl;              // tempo_de_vida
-    unsigned char protocol;         // identificacao_protocolo
+    unsigned char version;       // versao
+    unsigned char ihl;           // tamanho_cabecalho
+    unsigned char tos;           // tipo_servico
+    unsigned short tlen;         // comprimento_pacote
+    unsigned short id;           // identificacao
+    unsigned short flags;        // flags
+    unsigned short flags_offset; // deslocamento
+    unsigned char ttl;           // tempo_de_vida
+    unsigned char protocol;      // identificacao_protocolo
     unsigned short chsum;
-    unsigned int src;               // endereco_fisico_origem
-    unsigned int dst;               // endereco_fisico_destino
-    unsigned char *payload_ip;      // dados_ip
+    unsigned int src;          // endereco_fisico_origem
+    unsigned int dst;          // endereco_fisico_destino
+    unsigned char *payload_ip; // dados_ip
     size_t payload_size_ip;
 
     /* Pacote UDP */
-    unsigned short source_port;         // porta_origem
-    unsigned short destination_port;    // porta_destino
-    unsigned short size;                // tamanho_pacote
+    unsigned short source_port;      // porta_origem
+    unsigned short destination_port; // porta_destino
+    unsigned short size;             // tamanho_pacote
     unsigned short chechsum;
-    unsigned char *payload_udp;         // dados_udp
-    size_t payload_size_udp;                
+    unsigned char *payload_udp; // dados_udp
+    size_t payload_size_udp;
 };
